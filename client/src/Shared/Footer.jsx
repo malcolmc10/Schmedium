59fab2a98fc7623b527445ce0253116d07f687d5
Colin Malcolm <colinmalcolm8@gmail.com>
2020-07-20 18:48:59 -0400

    fixed professional links

--- client/src/Shared/Footer.jsx
+++ client/src/Shared/Footer.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 export default function Footer() {
     return (
         <div id="footer" className=" bg-gray-200 text-center mx-auto px-32 font-serif  py-4">
-           <p>Created by Colin Malcolm in July 2020</p> 
+            <p>Created by Colin Malcolm in July 2020</p>
 
-           <button href="https://www.linkedin.com/in/colinpmalcolm/" className="bg-red-800 hover:bg-blue-600 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4" >Linkedin</button>
+            <a href="https://www.linkedin.com/in/colinpmalcolm/"><button  className="bg-red-800 hover:bg-blue-600 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4">Linkedin</button></a>
 
-           <button href="https://github.com/malcolmc10/Schmedium" className="bg-red-800 hover:bg-black text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4">GitHub</button>
+            <a href="https://github.com/malcolmc10/Schmedium"><button  className="bg-red-800 hover:bg-black text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4">GitHub</button></a>
 
-           <button href="https://twitter.com/Themalcolmmind" className="bg-red-800 hover:bg-blue-300 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4">Twitter</button>
+            <a href="https://twitter.com/Themalcolmmind"><button className="bg-red-800 hover:bg-blue-300 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4">Twitter</button></a>
+            
         </div>
 
 
