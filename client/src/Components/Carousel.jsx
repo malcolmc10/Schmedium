d3b2eacfa6e13c73a8ce6177356e6b5c2e4414fe
Colin Malcolm <colinmalcolm8@gmail.com>
2020-07-20 20:17:32 -0400

    fix

--- client/src/Components/Carousel.jsx
+++ client/src/Components/Carousel.jsx
@@ -79,4 +79,4 @@ export default class SimpleSlider extends React.Component {
     }
   }
 
-//   used the react slider library to make the carousel.
+//   used the react slider library to make the carousel possible.
--- client/src/Shared/Footer.jsx
+++ client/src/Shared/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div id="footer" className=" bg-gray-200 text-center mx-auto px-32 font-serif  py-4">
             <p>Created by Colin Malcolm in July 2020</p>
 
-            <a href="https://www.linkedin.com/in/colinpmalcolm/"><button  className="bg-red-800 hover:bg-blue-600 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4">Linkedin</button></a>
+            <a href="https://www.linkedin.com/in/colinpmalcolm/"><button  className="bg-red-800 hover:bg-blue-600 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4">LinkedIn</button></a>
 
             <a href="https://github.com/malcolmc10/Schmedium"><button  className="bg-red-800 hover:bg-black text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline m-4">GitHub</button></a>
 
